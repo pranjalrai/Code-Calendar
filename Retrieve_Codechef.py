9aaf7c636d43f1563763f1986007b4930023d159
PRANJAL RAI <pranjal.rai125@gmail.com>
2018-11-08 10:29:50 +0530

    Update Retrieve_Codechef.py

--- Retrieve_Codechef.py
+++ Retrieve_Codechef.py
@@ -1,5 +1,5 @@
-#This snippet of code Retrieves data of contests from the file stored on the local system and arranges it in form of a 
-list of dictionaries where each dictionary corresponds to a single contest.
+#This snippet of code Retrieves data of contests from the file stored on the local system and arranges it in form of a list 
+#of dictionaries where each dictionary corresponds to a single contest.
 
 contest_list = []
 
